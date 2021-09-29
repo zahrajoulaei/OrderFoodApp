2a6d922b9fec5c83c4a4a85cad59ea5d3638c584
ZahraJou <zahrajoulaei@gmail.com>
2021-09-30 00:12:30 +0200

    create the header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,16 @@
-const Header=()=> {
+import { Fragment } from 'react';
+import foodimage from '../../assets/food.jpg';
+import classes from './Header.module.css';
 
+const Header = () => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>Order food with Zahra </h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={foodimage} alt="food picture"/>
+        </div>
+    </Fragment>
 }
 export default Header;
