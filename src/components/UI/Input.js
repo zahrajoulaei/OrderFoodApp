2f35edfc943b14bd7ba0b91fa363dea725068a10
ZahraJou <zahrajoulaei@gmail.com>
2021-10-10 22:32:43 +0200

    using Ref , forward ref and context

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,8 +1,14 @@
+import React from 'react';
+
 import classes from './Input.module.css';
-const Input = (props) => {
-    return <div className={classes.input}>
-        <label htmlfor={props.input.id}>{props.label}</label>
-        <input {...props.input}/>
+
+const Input = React.forwardRef((props, ref) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input ref={ref} {...props.input} />
     </div>
-}
+  );
+});
+
 export default Input;
