df421a06ba7e2bb40509044ca356e266410b9d8c
ZahraJou <zahrajoulaei@gmail.com>
2021-10-15 23:22:37 +0200

    work on Cart items

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,21 @@
 import { Fragment } from 'react';
-import foodimage from '../../assets/food.jpg';
-import classes from './Header.module.css';
+
 import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/food.jpg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>Order food with Zahra </h1>
-            <HeaderCartButton onClick={props.onShowCart}/>
-        </header>
-        <div className={classes['main-image']}>
-            <img src={foodimage} alt="food picture"/>
-        </div>
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
     </Fragment>
-}
+  );
+};
+
 export default Header;
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,11 +1,15 @@
-import MealsSummary from "./MealsSummary";
-import AvailableMeals from "./AvailableMeals";
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-const Meals = (props) => {
-    return <Fragment>
-        <MealsSummary />
-        <AvailableMeals />
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
     </Fragment>
-}
+  );
+};
+
 export default Meals;
